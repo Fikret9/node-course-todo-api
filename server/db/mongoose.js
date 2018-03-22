981b04821e6757e0969297a1eb17b95bc3a0b2ed
fikret <f_tokyay@yahoo.com>
2018-03-21 21:00:20 -0500

    SETUP APS FOR HEROKU

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 // Promise setup
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://fikret:fikret123@ds121599.mlab.com:21599/todoapp'
+};
+
+mongoose.connect(mlab || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/User');
 
 const {ObjectID} = require('mongodb');
+const port = process.env.PORT || 3000;
 
 
 var app = express();
@@ -58,8 +59,8 @@ app.get('/todos/:id',(req,res) => {
 
 
 
-app.listen(3000,() => {
-	console.log('Started on port 3000');
+app.listen(port,() => {
+	console.log(`Started on port ${port}`);
 })
  
 module.exports = {app};
