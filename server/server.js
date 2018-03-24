47d7166688e0d95604528e69c26cf148a82ca99a
fikret <f_tokyay@yahoo.com>
2018-03-23 20:52:56 -0500

    Update route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+const _  = require('lodash');
 
 //local imports
 var {mongoose} = require('./db/mongoose');
@@ -79,6 +80,33 @@ app.delete('/todos/:id',(req,res) => {
 
 })
 
+app.patch('/todos/:id',(req,res) => {
+	var id = req.params.id;
+	console.log(id);
+	var body = _.pick(req.body,['text','completed']);
+	if (!ObjectID.isValid(id)){	
+		return res.status(404).send();
+	}
+	if (_.isBoolean(body.completed) && body.completed){
+		body.completedAt = new Date().getTime();
+	} else {
+		body.completed = false;
+		body.completedAt = null;
+		}
+	Todo.findByIdAndUpdate(id,{$set: body},{new:true}).then((todo) => {
+		if(!todo){
+			res.status(404).send();
+		}
+		else
+		{
+			res.send(todo);
+		}
+		}).catch((e) => {
+			console.log(e);
+			res.status(400).send();
+		})
+			
+})
 
 
 app.listen(port,() => {
