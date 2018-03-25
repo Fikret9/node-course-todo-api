d4fb6297016297d81a9312788b2f4c0fa2a017eb
fikret <f_tokyay@yahoo.com>
2018-03-25 17:17:29 -0500

    gash use password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
  var data = {
  	id: 10
  };
 
 
-var token = jwt.sign(data,'123abc') ;
-console.log(token);
-var decoded = jwt.verify(token+'1','123abc');
-console.log(decoded);
+// var token = jwt.sign(data,'123abc') ;
+// console.log(token);
+// var decoded = jwt.verify(token+'1','123abc');
+// console.log(decoded);
 
+var hashpassword='';
+ var password  = '123abc!';
+ bcrypt.genSalt(10,(err,salt)=>{
+ 	bcrypt.hash(password,salt,(err,hash)=>{
+ 		console.log('1:' +hash);
+ 		hashpassword = hash; 
+ 	}); 	
+ })
+
+//var hashpassword = "$2a$10$O3iBqrQ95Ynhn7oyKsnXPeMfe45g6WxdlLEvSTKI7a2Muqi0n5gYa";
+bcrypt.compare(password,hashpassword,(err,result)=>{
+	console.log('2:  '+hashpassword);
+	console.log('3: '+result);
+});
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const _  = require('lodash');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/User');
-
+var {authenticate} = require('./middleware/authenticate');
 const {ObjectID} = require('mongodb');
 const port = process.env.PORT || 3000;
 
@@ -109,18 +109,37 @@ app.patch('/todos/:id',(req,res) => {
 
 app.post('/users',(req,res) => {
 	var body = _.pick(req.body,['email','password']);
-	var user = new User(body);
-	console.log('user1 '+user);
+	var user = new User(body); 
 	user.save().then(() => {
 		 return user.generateAuthToken();
-	}).then((token) => {
-		console.log('jfjfj');
+	}).then((token) => { 
 		res.header('x-auth',token).send(user);
 	}).catch((e) => {
 		console.log(e);
 		res.status(400).send(e);
 })
 });	
+
+var authenticate = (req,res,next ) => {
+var token = req.header('x-auth'); 
+ User.findByToken(token).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    req.user = user;
+    req.token = token;
+    next();
+  
+  }).catch((e) => {
+  	console.log(e);
+    res.status(401).send();
+  });
+};
+
+app.get('/users/me',authenticate,(req,res)=>{
+	res.send(req.user);
+})
 
 
 app.listen(port,() => {
